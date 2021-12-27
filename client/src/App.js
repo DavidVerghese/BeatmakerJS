af68b563e9947c568aa9c042548e62a5d3bd9ccb
David Verghese <dj.verghese@gmail.com>
2021-12-26 21:06:30 -0500

    created react app and basic interface

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Online Daw</h1>
+      <div className="drums">
+        <div className="kick">
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </div>
+        <div className="snare">
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
