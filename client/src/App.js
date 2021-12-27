c4c7c4d105186d937a454874fc78a6e854d20194
David Verghese <dj.verghese@gmail.com>
2021-12-27 12:46:57 -0500

    refactored app.js to be more dry by adding a inputgrid function

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import sequencer from './services/sequencer';
 import * as Tone from 'tone'
-
+import InputGrid from "./components/inputGrid"
 function App() {
 
   return (
@@ -10,86 +10,14 @@ function App() {
       <h1>Online Daw</h1>
       <button onClick={ async () => { await Tone.start(); console.log("context started");  sequencer();}}>start</button>
       <div className="drums">
-        <div className="kick">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="snare">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="hihat">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="rimshot">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="hightom">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="lowtom">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="handclap">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
-        <div className="crashcymbal">
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-          <input type="checkbox" />
-        </div>
+        <InputGrid name={"kick"} />
+        <InputGrid name={"snare"} />
+        <InputGrid name={"hihat"} />
+        <InputGrid name={"rimshot"} />
+        <InputGrid name={"hightom"} />
+        <InputGrid name={"lowtom"} />
+        <InputGrid name={"handclap"} />
+        <InputGrid name={"crashcymbal"}/>
       </div>
     </div>
   );
---
+++ client/src/components/inputGrid.jsx
@@ -0,0 +1,13 @@
+function inputGrid(props) {
+  return <div className={props.name}>
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+  <input type="checkbox" />
+</div>
+}
+export default inputGrid;
