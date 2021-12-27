87241dbf082bf4616705348e324c8987aa8d3911
David Verghese <dj.verghese@gmail.com>
2021-12-26 21:59:32 -0500

    Created a musicmaking interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import sequencer from './services/app';
+import * as Tone from 'tone'
 
 function App() {
 
-  
   return (
     <div >
       <h1>Online Daw</h1>
+      <button onClick={ async () => { await Tone.start(); console.log("context started");  sequencer();}}>start</button>
       <div className="drums">
         <div className="kick">
           <input type="checkbox" />
