524ef0b1b3b1a7c818469d9e9d6fa0f6f4774fa4
David Verghese <dj.verghese@gmail.com>
2021-12-28 16:44:39 -0500

    added in the note names

--- client/src/components/inputGrid.jsx
+++ client/src/components/inputGrid.jsx
@@ -2,14 +2,14 @@ import InputRow from './inputRow'
 
 function inputGrid(props) {
   return (<div className={props.className}>
-  <InputRow className={"zero"} />
-  <InputRow className={"one"} />
-  <InputRow className={"two"} />
-  <InputRow className={"three"} />
-  <InputRow className={"four"} />
-  <InputRow className={"five"} />
-  <InputRow className={"six"} />
-  <InputRow className={"seven"} />
+    <InputRow noteName={props.noteNamesArray[0]} className={"zero"} />
+  <InputRow noteName={props.noteNamesArray[1]} className={"one"} />
+  <InputRow noteName={props.noteNamesArray[2]} className={"two"} />
+  <InputRow noteName={props.noteNamesArray[3]} className={"three"} />
+  <InputRow noteName={props.noteNamesArray[4]} className={"four"} />
+  <InputRow noteName={props.noteNamesArray[5]} className={"five"} />
+  <InputRow noteName={props.noteNamesArray[6]} className={"six"} />
+  <InputRow noteName={props.noteNamesArray[7]} className={"seven"} />
 </div>)
 }
 export default inputGrid;
