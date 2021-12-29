3f65f3cd1d5ecf6de7665b6605a23dd2f1d0148d
David Verghese <dj.verghese@gmail.com>
2021-12-29 16:16:26 -0500

    created a footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+import './Footer.css';
+
+function Footer() {
+  return  <footer><div><div className="footer-pic"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBIxtAFjV-iTPm3DNMxVIFQVLdvHrwuEGrRQ&usqp=CAU" alt="github"/></div><a href="https://github.com/DavidVerghese" target="_blank" rel="noreferrer" ><p>Github</p></a></div><div><div><div className="footer-linkedin-pic"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0PfCg2MEeeIQ1ce0dgspPBX3b2Y4AL1_lVg&usqp=CAU" alt="linkedin"/></div> <a href="https://www.linkedin.com/in/davidjverghese/" target="_blank" rel="noreferrer" ><p>LinkedIn</p></a></div></div><div className="portfolio"><div className="footer-pic"><img src="https://davidverghese.surge.sh/static/media/CartoonMe.81832ec3.png" alt="portfolio logo"/></div> <a href="https://davidverghese.surge.sh/" target="_blank" rel="noreferrer" ><p>Portfolio</p></a></div></footer>
+}
+export default Footer;
