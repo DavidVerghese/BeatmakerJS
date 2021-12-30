70d3b0cb3d11ac301a777ce910c446ede6616181
David Verghese <dj.verghese@gmail.com>
2021-12-29 23:22:44 -0500

    changed order of instruments on page

--- client/src/App.js
+++ client/src/App.js
@@ -53,13 +53,14 @@ function App() {
      <Header/>
       <div className="musicmaker">
         <div className="topbar"><h2>How to Use This</h2><button onClick={() => { if (!startButtonClicked) { startSequencer() };setStartButtonClicked(true)}}>start</button> </div>
-        
+        <h3>Guitar</h3>
+      <InputGrid noteNamesArray={noteNamesC4ToC5} instrument={"lead"}/>
+        <h3>Bass</h3>
+        <InputGrid noteNamesArray={noteNamesC1ToC2} instrument={"bass"} />
         <h3>Drums</h3>
       <InputGrid noteNamesArray={noteNamesC1ToC2} instrument={"drums"}/>
-      <h3>Bass</h3>
-      <InputGrid noteNamesArray={noteNamesC1ToC2} instrument={"bass"}/>
-      <h3>Guitar</h3>
-      <InputGrid noteNamesArray={noteNamesC4ToC5} instrument={"lead"}/>
+      
+      
       </div>
       <Footer/>
     </div>
