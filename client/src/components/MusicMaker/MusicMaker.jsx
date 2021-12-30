022fd5fa9bca8dca14ffa1bac849611364dfa04b
David Verghese <dj.verghese@gmail.com>
2021-12-30 12:18:39 -0500

    Created a modal that plays a youtube video

--- client/src/components/MusicMaker/MusicMaker.jsx
+++ client/src/components/MusicMaker/MusicMaker.jsx
@@ -3,6 +3,9 @@ import sequencer from '../../services/sequencer';
 import * as Tone from 'tone'
 import InputGrid from "../InputGrid/InputGrid"
 import { useState } from "react";
+import Modal from 'react-modal';
+import YoutubeModal from '../YoutubeModal/YoutubeModal';
+
 import kickFile from '../../sounds/drums/kick.wav';
 import snareFile from '../../sounds/drums/snare.wav';
 import hihatFile from '../../sounds/drums/hihat.wav';
@@ -30,6 +33,8 @@ import leadE4File from '../../sounds/pipa/pipa-e4.wav';
 import leadD4File from '../../sounds/pipa/pipa-d4.wav';
 import leadC4File from '../../sounds/pipa/pipa-c4.wav';
 
+Modal.setAppElement('#root');
+
 const drumSoundsArray = [crashcymbalFile, handclapFile, lowTomFile, highTomFile, rimshotFile, hihatFile, snareFile, kickFile];
 const bassSoundsArray = [bassC2File,bassB1File,bassA1File,bassG1File,bassF1File,bassE1File,bassD1File,bassC1File];
 const leadSoundsArray = [leadC5File, leadB4File, leadA4File, leadG4File, leadF4File, leadE4File, leadD4File, leadC4File];
@@ -45,10 +50,12 @@ const noteNamesC4ToC5 = ["C5", "B4", "A4", "G4", "F4", "E4", "D4", "C4"];
 
 function MusicMaker() {
   const [startButtonClicked, setStartButtonClicked] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (    <div className="musicmaker">
-  <div className="topbar"><h2>How to Use This</h2><button onClick={() => { if (!startButtonClicked) { startSequencer() };setStartButtonClicked(true)}}>start</button> </div>
-  <h3>Guitar</h3>
+  <div className="topbar"><button onClick={()=> setModalIsOpen(true)}>How to Use This</button><button onClick={() => { if (!startButtonClicked) { startSequencer() };setStartButtonClicked(true)}}>start</button> </div>
+    <YoutubeModal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}/>
+    <h3>Guitar</h3>
 <InputGrid noteNamesArray={noteNamesC4ToC5} instrument={"lead"}/>
   <h3>Bass</h3>
   <InputGrid noteNamesArray={noteNamesC1ToC2} instrument={"bass"} />
