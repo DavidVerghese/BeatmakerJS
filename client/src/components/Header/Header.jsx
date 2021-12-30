3d078744a63ac941c9ce2be77328dfa561ed01de
David Verghese <dj.verghese@gmail.com>
2021-12-30 18:00:32 -0500

    changed website title

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import './Header.css'
 
 function Header(props) {
-  return  <header><h1>Online Daw</h1></header>
+  return  <header><h1>David Verghese's Online Daw</h1></header>
 }
 export default Header;
