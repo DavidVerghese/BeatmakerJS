d4fe73cb6b442be02bb4543244713c71535d0e49
David Verghese <dj.verghese@gmail.com>
2022-01-10 17:49:06 -0500

    brought back get route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
   before_action :authorize_request, except: :create
 
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+  
   # POST /users
   def create
     @user = User.new(user_params)
