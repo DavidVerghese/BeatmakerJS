0833903245fc2af151a9e71f8b6203fe46ec1dff
David Verghese <dj.verghese@gmail.com>
2022-01-10 16:32:00 -0500

    Setup frontend files for working with api

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,23 @@
+
+
+import axios from 'axios'
+
+const getToken = () => {
+  return new Promise(resolve => {
+    resolve(`Bearer ${localStorage.getItem('token') || null}`)
+  })
+}
+
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://onlinedaw-api.herokuapp.com' : 'http://localhost:3000/'
+})
+
+api.interceptors.request.use(async function (options) {
+  options.headers['Authorization'] = await getToken()
+  return options
+}, function (error) {
+  console.log('Request error: ', error)
+  return Promise.reject(error)
+});
+
+export default api
