075a9e2f5b5f1245961e8085083f69eef12b00be
David Verghese <dj.verghese@gmail.com>
2022-01-11 17:43:12 -0500

    added sign in and sign up to header

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -1,4 +1,5 @@
 import api from './apiConfig'
+import jwtDecode from 'jwt-decode'
 
 export const getUser = async id => {
   try {
@@ -9,3 +10,51 @@ export const getUser = async id => {
   }
 }
 
+export const signUp = async credentials => {
+    try {
+        const resp = await api.post('/sign-up', credentials)
+        localStorage.setItem('token', resp.data.token)
+        const user = jwtDecode(resp.data.token)
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+export const signIn = async credentials => {
+    try {
+        const resp = await api.post('/sign-in', credentials)
+        localStorage.setItem('token', resp.data.token)
+        const user = jwtDecode(resp.data.token)
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+export const signOut = async user => {
+    try {
+        localStorage.clear()
+        return true
+    } catch (error) {
+        throw error
+    }
+}
+
+export const changePassword = async (passwords, user) => {
+    try {
+        const resp = await api.post('/')
+        return resp.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const verifyUser = async () => {
+    const token = localStorage.getItem('token')
+    if (token) {
+        const res = await api.get('/verify')
+        return res.data
+    }
+    return false
+}
