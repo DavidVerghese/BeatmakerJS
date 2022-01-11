8e9498f4af213d9bbcf7779a38e86f5925bc6414
David Verghese <dj.verghese@gmail.com>
2022-01-11 18:27:13 -0500

    worked on systems on how links should open in new tab

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import './Header.css'
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header(props) {
   let user = props.user;
+  const [hasUserClickedBlog, setHasUserClickedBlog] = useState(false);
   return <header>
     <Link to="/" target="_blank" rel="noopener noreferrer" ><p>David Verghese's Online Daw</p></Link>
-    {user ? <p>Sign Out</p> : <><Link to="/sign-in" target="_blank" rel="noopener noreferrer" ><p>Sign in</p></Link>
+    {user ? <p>Sign Out</p> : (hasUserClickedBlog ? <><Link to="/sign-in" target="_blank" rel="noopener noreferrer" ><p>Sign in</p></Link>
       <Link to="/sign-up" target="_blank" rel="noopener noreferrer" ><p>Sign up</p></Link>
-    </>}
-    <Link to="/blog" target="_blank" rel="noopener noreferrer" >Blog</Link>
+    </> : null)}
+    {hasUserClickedBlog ? <Link to="/blog"><p>Blog</p></Link> :  <Link to="/blog" target="_blank" rel="noopener noreferrer"><p onClick={() => { setHasUserClickedBlog(true) }}>Blog</p></Link> }
   </header>
 }
 export default Header;
