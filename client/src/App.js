503dee51a5bb55e5f1d6a343c061ca671895d10e
David Verghese <dj.verghese@gmail.com>
2022-01-11 17:51:53 -0500

    Added sign-up component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
-
 import { useState, useEffect } from "react";
 import { verifyUser } from './services/users'
 import { Route } from "react-router-dom";
 import Home from './screens/Home/Home';
 import Blog from './screens/Blog/Blog';
+import SignUp from "./screens/SignUp/SignUp";
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+
 
 function App() {
   const [user, setUser] = useState(false);
@@ -24,6 +25,8 @@ function App() {
       <Header user={user}/>
       <Route exact path="/blog"><Blog/></Route>
       <Route exact path="/"><Home /></Route>
+      <Route exact path="/sign-up"><SignUp setUser={setUser}/></Route>
+      <Route exact path="/sign-in"><Home /></Route>
       <Footer/>
     </div>
   );
---
+++ client/src/screens/SignUp/SignUp.jsx
@@ -0,0 +1,114 @@
+import { signUp, signIn } from "../../services/users";
+import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
+
+function SignUp(props) {
+  const history = useHistory();
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    isError: false,
+    errorMsg: "",
+  });
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+
+  const onSignUp = (event) => {
+    event.preventDefault();
+    const { setUser } = props;
+    
+    signUp(form)
+      .then(() => signIn(form))
+      .then((user) => setUser(user))
+      .then(() => history.push("/"))
+      .catch((error) => {
+        console.error(error);
+        setForm({
+          email: "",
+          password: "",
+          passwordConfirmation: "",
+          isError: true,
+          errorMsg: "Sign Up Details Invalid",
+        });
+      });
+  };
+
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return (
+        <button id="signup-button" type="submit">
+          Sign Up
+        </button>
+      );
+    }
+  };
+
+  const { email, username, password, passwordConfirmation } = form;
+
+  return ( <div className="signup-background">
+     
+  <div className="signup-container">
+    <h1>Sign Up</h1>
+    <form className="signup-form" onSubmit={onSignUp}>
+    <label htmlFor="signup-username">Username</label>
+        <input
+        required
+        type="text"
+          name="username"
+        value={username}
+        placeholder="Enter username"
+        onChange={handleChange}
+        id="signup-username"
+        />
+         <label htmlFor="signup-email">Email</label>
+      <input
+        required
+        type="email"
+        name="email"
+        value={email}
+        placeholder="Enter email"
+        onChange={handleChange}
+        id="signup-email"
+        />
+         <label htmlFor="signup-password">Password</label>
+      <input
+        required
+        name="password"
+        value={password}
+        type="password"
+        placeholder="Password"
+        onChange={handleChange}
+        id="signup-password"
+        />
+         <label htmlFor="signup-password-confirmation">Password confirmation</label>
+      <input
+        required
+        name="passwordConfirmation"
+        value={passwordConfirmation}
+        type="password"
+        placeholder="Confirm Password"
+        onChange={handleChange}
+        id="signup-password-confirmation"
+      />
+      {renderError()}
+    </form>
+  </div>
+  </div>)
+}
+export default SignUp
